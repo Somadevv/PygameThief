fb2fee206fee010847d3598e9560c8aa18306a55
fuglyuckDev <harrisoncwalton@gmail.com>
2023-11-29 23:06:30 +0000

    Large Update
    Project was abandoned.
    Commit is just to update the Repo to it's current state.
    Will maybe pickup in the future.

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ from scripts.shophandler import Shop, Button
 # Initialise pygame
 pygame.init()
 
-
 # Draw Screen
-SCREEN_WIDTH, SCREEN_HEIGHT = 750, 500
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 CANVAS = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 GAME_WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # FPS Params
 CLOCK = pygame.time.Clock()
 TARGET_FPS = 60
-GAME_TICK = 120
+GAME_TICK = 240
 
 # Run Game Bool
 running = True
@@ -42,21 +41,20 @@ shopHandlerButton.button_rects()
 
 
 # Player Position on Load
-playerInstance.position.x, playerInstance.position.y = 0, 0
+playerInstance.position.x, playerInstance.position.y = 10, 50
 
 # Game loop
 while running:
-    # Draw World
-
-    worldGeneration.DrawWorld(CANVAS, generateWorld.rects)
-    GAME_WINDOW.blit(CANVAS, (0, 0))
-
     # Define Delta Time
     dt = CLOCK.tick(GAME_TICK) * .001 * TARGET_FPS
 
     # Control
     playerControls.GameControls(
         playerInstance, generateWorld.rects, playerInventory, shopHandler, shopHandlerButton)
+
+    # Draw World
+    worldGeneration.DrawWorld(CANVAS, generateWorld.rects)
+    GAME_WINDOW.blit(CANVAS, (0, 0))
 
     # Update Player Position
     playerInstance.initialize(dt, generateWorld.rects)
